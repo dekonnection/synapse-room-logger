1ab73c2a6bce840a29137b1bd82e78415c941298
Cédric Laudrel <dek@iono.me>
2018-10-29 12:56:05 +0100

    reformat using Black

--- synapse-room-logger.py
+++ synapse-room-logger.py
@@ -31,7 +31,9 @@ class SynapseRoomLogger(object):
         self.db_password = config["db_password"]
         self.output_directory = config["output_directory"]
         state_file_name = ".last_ts"
-        self.state_file_path = '{}/{}'.format(config['output_directory'], state_file_name)
+        self.state_file_path = "{}/{}".format(
+            config["output_directory"], state_file_name
+        )
 
     def db_connect(self):
         logging.info("Connecting to database ...")
@@ -41,14 +43,16 @@ class SynapseRoomLogger(object):
                 database=self.db_name,
                 user=self.db_user,
                 password=self.db_password,
-                connect_timeout=5
+                connect_timeout=5,
             )
             self.conn = conn
             self.cur = self.conn.cursor()
             logging.info("Connected to database.")
             return True
         except psycopg2.OperationalError as e:
-            logging.error("Could not connect to database : \"{}\"".format(str(e).rstrip()))
+            logging.error(
+                'Could not connect to database : "{}"'.format(str(e).rstrip())
+            )
             return False
 
     def db_disconnnect(self):
@@ -56,29 +60,36 @@ class SynapseRoomLogger(object):
         self.cur.close()
         self.conn.close()
         logging.info("Disconnected from database.")
-        
 
     def request_messages(self):
         base_query = "SELECT e.received_ts, j.json FROM events AS e INNER JOIN event_json AS j USING (event_id) WHERE e.room_id='{room_id}' AND e.received_ts>{after_ts} AND e.type='m.room.message' ORDER BY e.received_ts;"
 
         self.read_last_ts_written()
-        
+
         if not self.db_connect():
             logging.info("We won't do anything this time")
             return False
 
-        self.cur.execute(base_query.format(room_id=self.room_id, after_ts=self.last_ts_written))
+        self.cur.execute(
+            base_query.format(room_id=self.room_id, after_ts=self.last_ts_written)
+        )
 
         for row in self.cur:
             # there is two fields : timestamp at reception, and json data
             line = self.process_message_row(row)
-            file_path = self.ts_to_filepath(line['ts'])
+            file_path = self.ts_to_filepath(line["ts"])
 
             if self.append_line(file_path, json.dumps(line)):
-                logging.info("Message with timestamp {} written to {}".format(line['ts'], file_path))
-                self.last_ts_written = line['ts']
+                logging.info(
+                    "Message with timestamp {} written to {}".format(
+                        line["ts"], file_path
+                    )
+                )
+                self.last_ts_written = line["ts"]
             else:
-                logging.error("We couldn't write message {}, we will exit.".format(line['ts']))
+                logging.error(
+                    "We couldn't write message {}, we will exit.".format(line["ts"])
+                )
                 return False
         self.write_last_ts_written()
         self.db_disconnnect()
@@ -109,10 +120,18 @@ class SynapseRoomLogger(object):
                 file.write(line + "\n")
                 return True
         except FileNotFoundError:
-            logging.error("Could not write {}, maybe the parent directory is missing.".format(file_path))
+            logging.error(
+                "Could not write {}, maybe the parent directory is missing.".format(
+                    file_path
+                )
+            )
             return False
         except PermissionError:
-            logging.error("Could not write {}, you don't have sufficient permissions to write here.".format(file_path))
+            logging.error(
+                "Could not write {}, you don't have sufficient permissions to write here.".format(
+                    file_path
+                )
+            )
             return False
 
     def read_last_ts_written(self):
@@ -120,11 +139,19 @@ class SynapseRoomLogger(object):
             logging.info("Reading last timestamp written from previous run.")
             with open(self.state_file_path, "r") as file:
                 self.last_ts_written = int(file.read())
-                logging.info("Last timestamp from previous run is {}.".format(self.last_ts_written))
+                logging.info(
+                    "Last timestamp from previous run is {}.".format(
+                        self.last_ts_written
+                    )
+                )
                 return True
         except FileNotFoundError:
             self.last_ts_written = 0
-            logging.warn("No state file found at {}, setting last timestamp written to 0.".format(self.state_file_path))
+            logging.warn(
+                "No state file found at {}, setting last timestamp written to 0.".format(
+                    self.state_file_path
+                )
+            )
             return False
 
     def write_last_ts_written(self):
@@ -132,27 +159,40 @@ class SynapseRoomLogger(object):
             logging.info("Writing last timestamp to state file.")
             with open(self.state_file_path, "w") as file:
                 file.write(str(self.last_ts_written))
-                logging.info("Timestamp {} written to state file.".format(self.last_ts_written))
+                logging.info(
+                    "Timestamp {} written to state file.".format(self.last_ts_written)
+                )
                 return True
         except FileNotFoundError:
-            logging.error("Could not write {}, maybe the parent directory is missing.".format(file_path))
+            logging.error(
+                "Could not write {}, maybe the parent directory is missing.".format(
+                    file_path
+                )
+            )
             return False
         except PermissionError:
-            logging.error("Could not write {}, you don't have sufficient permissions to write here.".format(file_path))
+            logging.error(
+                "Could not write {}, you don't have sufficient permissions to write here.".format(
+                    file_path
+                )
+            )
             return False
-
 
     def cron(self):
         pass
 
-def main():
-    arguments = docopt(__doc__, version='0.1')
 
-    if arguments['--debug']:
-        log_level='DEBUG'
+def main():
+    arguments = docopt(__doc__, version="0.1")
+
+    if arguments["--debug"]:
+        log_level = "DEBUG"
     else:
-        log_level = config['log_level']
-    logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(message)s',level=logging.getLevelName(log_level))
+        log_level = config["log_level"]
+    logging.basicConfig(
+        format="%(asctime)s :: %(levelname)s :: %(message)s",
+        level=logging.getLevelName(log_level),
+    )
 
     logging.debug("Arguments : {}".format(json.dumps(arguments)))
     logging.info("Starting Matrix room logger.")
