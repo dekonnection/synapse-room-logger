4ab6b932a1d1c21cc004ddb7eaabf5f727d36c17
Cédric Laudrel <dek@iono.me>
2018-10-31 15:13:51 +0100

    docker: use python base image instead of stock alpine

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@ srl_conf_file=/synapse-room-logger/config.yaml
 if [ -f "$srl_conf_file" ]
 then
     echo "OK : starting synapse-room-logger"
-    /usr/bin/python3 -m synapse-room-logger daemon
+    /usr/local/bin/python -m synapse-room-logger daemon
 else
     echo -e "ERROR : you need to mount a bind volume to expose \
 $srl_conf_file to the container.\nWe will now exit."
