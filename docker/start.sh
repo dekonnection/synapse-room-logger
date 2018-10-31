797c07598e06e12ac7f96a6cfa49846fc2b2516b
Cédric Laudrel <dek@iono.me>
2018-10-31 14:59:44 +0100

    add Dockerfile

---
+++ docker/start.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+srl_conf_file=/synapse-room-logger/config.yaml
+
+if [ -f "$srl_conf_file" ]
+then
+    echo "OK : starting synapse-room-logger"
+    /usr/bin/python3 -m synapse-room-logger daemon
+else
+    echo -e "ERROR : you need to mount a bind volume to expose \
+$srl_conf_file to the container.\nWe will now exit."
+    exit 1
+fi
